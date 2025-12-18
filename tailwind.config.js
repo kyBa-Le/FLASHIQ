59bacd6cbde77380e3d8252e09c4725f996d2c84
Hien Cao <hien.cao@zimaw.com>
2025-12-18 17:43:59 +0700

    chore: config taiwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#7132CA",
+        secondary: "#C47BE4",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      borderRadius: {
+        sm: "calc(var(--radius) - 4px)",
+        md: "calc(var(--radius) - 2px)",
+        lg: "var(--radius)",
+      },
+    },
   },
+  plugins: [],
 };
