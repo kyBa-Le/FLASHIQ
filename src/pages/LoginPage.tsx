14206b6191cee0d77e4142345b7bbb8c0f97d5e6
Xabe14 <bsa30012@gmail.com>
2025-12-19 14:28:02 +0700

    * chore(deps): setup shadcn
    * chore(deps): setup shadcn
    
    * chore(deps): setup shadcn

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/Button";
+import Cookies from "js-cookie";
+
+const Login = () => {
+  const login = () => {
+    Cookies.set("access_token", "fake-token");
+    window.location.href = "/";
+  };
+
+  return (
+    <div className="flex flex-col bg-red-500">
+      <h1>Login</h1>
+      <Button variant="outline" onClick={login}>Login</Button>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">404 – Not Found</h1>
+      <h1 className="text-2xl font-bold text-red-500">404 – Not Found</h1>
     </div>
   );
 }
